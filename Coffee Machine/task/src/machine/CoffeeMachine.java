1c5dd6d9dd051c6d745dfd548597df110843323f
Zino Adidi <zinoadidi@gmail.com>
2019-06-12 13:27:49 +0300

    added fix for coffee detection

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -3,9 +3,9 @@ package machine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    private static int water = 200;
-    private static int milk = 50;
-    private static int coffee = 15;
+    private static int waterRequirement = 200;
+    private static int milkRequirement = 50;
+    private static int coffeeRequirement = 15;
 
 
 
@@ -21,27 +21,63 @@ public class CoffeeMachine {
         // object should contain values suck as
 
         // variables decleared below can remain for Accepting user inputs
-//        int cups;
-//        int water = 200;
-//        int milk = 50;
-//        int coffee = 15;
+        int cups;
+        int water;
+        int milk;
+        int coffee;
+
+        int caculationResult;
+        int cupsOfCoffeePossible;
 
         /// collect values for variables above
         Scanner input = new Scanner(System.in);
 
+// accept input
 
-
+        System.out.print("Write how many ml of water the coffee machine has: ");
+        water = input.nextInt();
+        System.out.print("Write how many ml of milk the coffee machine has: ");
+        milk = input.nextInt();
+        System.out.print("Write how many grams of coffee beans the coffee machine has: ");
+        coffee = input.nextInt();
         System.out.print("Write how many cups of coffee you will need: ");
         cups = input.nextInt();
-        water = cups * water;
-        milk = cups * milk;
-        coffee = cups * coffee;
 
 
-        System.out.println("For " + cups + " cups of coffee you will need: " );
-        System.out.println(water + " ml of Water");
-        System.out.println(milk + " ml of milk");
-        System.out.println(coffee + " g of coffee beans ");
+
+
+   // calculate
+
+
+        // check if adequate requirement
+
+        if(water => waterRequirement){
+            calculationResult = true;
+        }else{
+            calculationResult = false;
+        }
+        if(milk => milkRequirement){
+            calculationResult = true;
+        }else{
+            calculationResult = false;
+        }
+        if(coffee => coffeeRequirement){
+            calculationResult = true;
+        }else{
+            calculationResult = false;
+        }
+
+
+/// results
+
+        if(calculationResult){
+            System.out.println("Yes, I can make that amount of coffee ");
+
+        }else{
+            System.out.println("No, I can make only" +cupsOfCoffeePossible+ "cup(s) of coffee");
+        }
 
     }
+
+
 }
