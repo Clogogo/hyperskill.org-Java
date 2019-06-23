4126b5cf0d70fc0c83f32acdc7f299237ebc99cc
lucky <chigoziecigo@yahoo.com>
2019-06-23 16:10:15 +0300

    Task 3 Solved

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,12 +1,14 @@
 package machine;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CoffeeMachine {
+
+
     private static int waterRequirement = 200;
     private static int milkRequirement = 50;
     private static int coffeeRequirement = 15;
-
 
 
     public static void main(String[] args) {
@@ -14,7 +16,6 @@ public class CoffeeMachine {
         // fit the requirements to make one cup of coffee as showned below
         // "Like in the previous stage, the coffee machine needs 200 ml of water,
         // 50 ml of milk, and 15 g of coffee beans to make one cup of coffee."
-
 
 
         // make an object that contain the result;
@@ -26,8 +27,6 @@ public class CoffeeMachine {
         int milk;
         int coffee;
 
-        int caculationResult;
-        int cupsOfCoffeePossible;
 
         /// collect values for variables above
         Scanner input = new Scanner(System.in);
@@ -43,40 +42,28 @@ public class CoffeeMachine {
         System.out.print("Write how many cups of coffee you will need: ");
         cups = input.nextInt();
 
+        int milkAvail = milk / milkRequirement;
+        int waterAvail = water / waterRequirement;
+        int coffeeAvail = coffee / coffeeRequirement;
+        // calculate
+        int[] maxValve = {milkAvail, waterAvail, coffeeAvail};
+        Arrays.sort(maxValve);
 
 
+        int numOfcupsAva = maxValve[0];
 
-   // calculate
+        //int numOfcupsAva = milkAvail + waterAvail + coffeeAvail / 3;
+        //If else Option
 
 
-        // check if adequate requirement
-
-        if(water => waterRequirement){
-            calculationResult = true;
-        }else{
-            calculationResult = false;
+        if (numOfcupsAva == cups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (numOfcupsAva > cups) {
+            int makeMore = numOfcupsAva - cups;
+            System.out.println("Yes, I can make that amount of coffee (and even " + makeMore + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + numOfcupsAva + "  cup(s) of coffee");
         }
-        if(milk => milkRequirement){
-            calculationResult = true;
-        }else{
-            calculationResult = false;
-        }
-        if(coffee => coffeeRequirement){
-            calculationResult = true;
-        }else{
-            calculationResult = false;
-        }
-
-
-/// results
-
-        if(calculationResult){
-            System.out.println("Yes, I can make that amount of coffee ");
-
-        }else{
-            System.out.println("No, I can make only" +cupsOfCoffeePossible+ "cup(s) of coffee");
-        }
-
     }
 
 
