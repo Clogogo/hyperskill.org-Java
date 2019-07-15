369cf1ede449e59d0f46814c2c3a76f2cd15ab93
lucky <chigoziecigo@yahoo.com>
2019-07-15 11:55:27 +0300

    Simple chatty bot task 1

---
+++ Simple Chatty Bot (Java)/Simple Chatty Bot (Java)/task/src/bot/SimpleBot.java
@@ -0,0 +1,13 @@
+package bot;
+
+import java.util.Date;
+
+public class SimpleBot {
+
+    public static void main(String[] args) {
+        String botname = "bot001";
+        String date = "2018";
+        System.out.println("Hello! My name is " + botname);
+        System.out.println("I was created in " + date );
+    }
+}
