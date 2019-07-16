4b24992a8beaaa77d76941be363b09d47b9caf2e
lucky <chigoziecigo@yahoo.com>
2019-07-16 17:31:39 +0300

    Simple chatty bot task 4

--- Simple Chatty Bot (Java)/Simple Chatty Bot (Java)/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot (Java)/Simple Chatty Bot (Java)/task/src/bot/SimpleBot.java
@@ -12,20 +12,21 @@ public class SimpleBot {
         int ans1;
         int ans2;
         String name;
+        int choice;
 
 
         System.out.println("Hello! My name is " + botname);
         System.out.println("I was created in " + date);
         System.out.println("Please, remind me your name : ");
 
-        name = input.nextLine();
+        name = input.nextLine(); // input Name
 
         System.out.println("What a great name you have " + name);
         System.out.println("Let me guess your age:. ");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
-        ans = input.nextInt();
-        ans1 = input.nextInt();
-        ans2 = input.nextInt();
+        ans = input.nextInt();          // input Reminder of dividing by 3
+        ans1 = input.nextInt();             // input Reminder of dividing by 5
+        ans2 = input.nextInt();         // input Reminder of dividing by 7
 
 
         int calAge = ((ans % 3) * 70 + (ans1 % 5) * 21 + (ans2 % 7) * 15) % 105;
@@ -36,7 +37,30 @@ public class SimpleBot {
         for (int i = 0; i <= number; i++) {
             System.out.println(+i + "!");
         }
-        System.out.println("completed, have a nice day!");
+
+        //Printing Out Question
+        System.out.println();
+        System.out.println("Let's test your programming knowledge :");
+        System.out.println("Why do we use methods : ");
+        System.out.println("1. To repeat a statement multiple times. " +
+                "\n2. To decompose a program into several small subroutines. " +
+                "\n3. To determine the execution time of a program. " +
+                "\n4. To interrupt the execution of a program.");
+        System.out.println();
+
+        // Do while loop for the questions
+        do {
+            choice = input.nextInt();
+
+            if (choice != 2) {
+                System.out.println("Please, try again.");
+            }
+        } while (choice != 2);
+        System.out.println("Congratulations, have a nice day!");
+
 
     }
+
+
 }
+
